75e1d9b3355bd92085dd7e047cc870ddf68c6241
沈羿伶 <asd565437@Mac.mshome.net>
2025-02-11 22:48:16 +0800

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEBOvR5IsLUspq0AGF12wQWXA69-XpBxI",//你的API金鑰
+  authDomain: "biolink-auth.firebaseapp.com",//你的專案ID.firebaseapp.com
+  projectId: "biolink-auth",//你的專案ID
+  messagingSenderId: "507593072695",//你的訊息發送者ID
+  appId: "1:507593072695:web:d6e8d53729083bc7d91fb2"//你的應用程式ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+export const db = getFirestore(app);
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
+
+export const googleProvider = new GoogleAuthProvider();

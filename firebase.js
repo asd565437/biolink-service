56ef40f20fda0293571bb1bc569de4e053fdbef1
沈羿伶 <asd565437@Mac.mshome.net>
2025-02-12 22:57:20 +0800

    Convert Firebase module to CommonJS

--- firebase.js
+++ firebase.js
@@ -1,27 +1,25 @@
+require("dotenv").config();
 const { initializeApp } = require("firebase/app");
 const { getAuth, GoogleAuthProvider } = require("firebase/auth");
 const { getFirestore } = require("firebase/firestore");
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase 配置
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID
 };
-
-// Initialize Firebase
+// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore Database
+// 初始化 Firestore 数据库
 const db = getFirestore(app);
 
-// Initialize Firebase Authentication
+// 初始化 Firebase 认证
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-module.exports = { db, auth, googleProvider, firebaseConfig};
+module.exports = { db, auth, googleProvider, firebaseConfig };

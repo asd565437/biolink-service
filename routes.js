ec812eb939dc22f548ba02c869608fe3f0761c73
沈羿伶 <asd565437@chenyilingdeMacBook-Air.local>
2025-02-13 22:41:18 +0800

    Convert Firebase module to CommonJS

--- routes.js
+++ routes.js
@@ -45,7 +45,7 @@ router.post('/register', async (req, res) => {
       googleLogin
     });
 
-    res.status(201).json({ message: '註冊成功', user: { id: user_id } });
+    res.status(200).json({ message: '註冊成功', user: { id: user_id } });
   } catch (error) {
     console.error('Error during registration:', error);
     res.status(500).json({ error: 'Internal server error' });

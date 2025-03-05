c27ae37ec3fadaf88653bbba91d1385d6a9b9ddf
asd565437 <asd565437@gmail.com>
2025-03-05 16:45:55 +0800

    Fix requirements.txt

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ async function sendFilePath(filePath) {
       const response = await axios.post(`https://biolink-py-server.onrender.com/process`, {
           file_path: filePath
       });
-
+      console.log(filePath)
       console.log("Flask 回應:", response.data);
   } catch (error) {
       console.error("請求 Flask 時發生錯誤:", error.message);

d1d3db25e9ed21636c1392f5c84bcdbf00439544
asd565437 <asd565437@gmail.com>
2025-04-05 03:04:21 +0800

    first commit

--- server.js
+++ server.js
@@ -201,13 +201,6 @@ app.post("/data", async (req, res) => {
     return res.status(500).json({ error: "伺服器錯誤" });
   }
 });
-app.post("/webhook", async (req, res) => {
-  try {
-    console.log(req.body);
-  } catch (error) {
-    console.error("設定 Cookie 失敗:", error);
-  }
-});
 
 app.use("/api", routes);
 
